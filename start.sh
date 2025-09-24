9663c6db9fe340c465c7cf136ca02a2432a3aa13
Francesco Palazzo <francesco@palazzo.me>
2025-09-24 16:16:25 +0200

    ZSH compatibility

--- start.sh
+++ start.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
 export PRIMAPARTE=''
 export SECONDAPARTE=''
 export TERZAPARTE=''
-export TIMESTART=`date +%s%6N`
+export TIMESTART=$(date +%s%6N)
 
-set +o history
-export HISTFILE=/dev/null
-export HISTSIZE=0
-export HISTFILESIZE=0
+
+if [ -n "$ZSH_VERSION" ]; then
+  fc -p
+  fc -R /dev/null
+elif [ -n "$BASH_VERSION" ]; then
+  set +o history
+  export HISTFILE=/dev/null
+  export HISTSIZE=0
+  export HISTFILESIZE=0
+fi
 
 echo "Timer start. Fai del tuo meglio."
